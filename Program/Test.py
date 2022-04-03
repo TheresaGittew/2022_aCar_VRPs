5543091ac85165daabbb74cb8109608ee7bd8914
theresawettig <theresa.wettig@gmx.de>
2022-04-03 22:12:52 +0200

    Several changes (valid inequalities, removal of battery constraint, experimental study)

--- Program/Test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-output = subprocess.check_output([sys.executable, '/Users/theresawettig/PycharmProjects/2022_aCar_VRPs/Program/Execution.py'])
-with open('03-21-large.txt', 'wb') as outfile:
-    outfile.write(output)
